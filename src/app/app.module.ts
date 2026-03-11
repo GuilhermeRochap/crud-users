7f961520341aaca682e57a1bc3362b449eded921
Guilherme Rocha <guirpessanha@gmail.com>
2026-03-11 13:27:00 -0300

    Angular CRUD USERS Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginComponent } from './pages/login/login.component';
+import { ButtonComponent } from './components/button/button.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { CrudComponent } from './pages/crud/crud.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ButtonComponent,
+    HomeComponent,
+    MenuComponent,
+    UsuariosComponent,
+    CrudComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig : {
+  apiKey: "AIzaSyBSYKr39P4jvzd9N6gmEsIgnG3d6nwcQbA",
+  authDomain: "crud-users-2bc50.firebaseapp.com",
+  projectId: "crud-users-2bc50",
+  storageBucket: "crud-users-2bc50.firebasestorage.app",
+  messagingSenderId: "875185531866",
+  appId: "1:875185531866:web:7d6acd571d86c113a6eee3",
+  measurementId: "G-J9Y8KKMFN9"
+}
+};
